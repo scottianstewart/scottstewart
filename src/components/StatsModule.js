85840b3e04caf578a578191b433f0a1447fbb51f
Scott Stewart <scott@scottstewart.co>
2023-03-30 12:47:22 -0700

    commited

--- src/components/StatsModule.js
+++ src/components/StatsModule.js
@@ -21,11 +21,11 @@ const StatsModule = () => {
     var m = Math.floor((seconds % 3600) / 60);
     var s = Math.floor(seconds % 60);
 
-    var dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
-    var hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
-    var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
-    var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
-    return dDisplay.toLocaleString("en-us") + hDisplay + mDisplay + sDisplay;
+    var dDisplay = d > 0 ? d.toLocaleString("en-us") + "d " : "";
+    var hDisplay = h > 0 ? h + "h " : "";
+    var mDisplay = m > 0 ? m + "m " : "";
+    var sDisplay = s >= 0 ? s + "s" : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
   }
 
   return (
